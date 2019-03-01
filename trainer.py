dd8f621a3993d1cb2e0fd388a1793ca6748cf58d
RobinC94 <sissure@qq.com>
2019-03-01 15:37:04 +0800

    Initial commit

---
+++ trainer.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+# -*-coding:utf-8-*-
+
+import os
+import argparse
+import tensorflow as tf
+
+parser = argparse.ArgumentParser()
+
+args, _ = parser.parse_known_args()
+
+
+if __name__ == '__main__':
+    pass
